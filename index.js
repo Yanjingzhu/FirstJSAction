543c89058a8aa6fe82b6959639ea30812b6ceff1
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-04-29 17:47:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
+const cp = require('child_process');
+
+cp.execSync(`cd ${__dirname}; yarn install`);
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  core.setFailed("Errors setFailed");
-  process.exit(1);
-} catch (error) {
-  core.setFailed(error.message);
+async function tag() {
+
+    const token = core.getInput("token", { required: true });
+    const tagName = core.getInput("tag", { required: true });
+    const tagMessage = core.getInput("message", { required: true });
+    const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split("/");
+
+    const oct = new github.GitHub(token);
+
+    const createdTag = await oct.git.createTag({
+        owner: repoOwner,
+        repo: repoName,
+        tag: tagName,
+        message: tagMessage,
+        object: process.env.GITHUB_SHA,
+        type: "commit"
+    });
+
+    if (createdTag.status !== 201) {
+        core.setFailed(`Could not create tag. Received ${createdTag.status} from API`)
+    }
+
+    await oct.git.createRef({
+        owner: repoOwner,
+        repo: repoName,
+        ref: 'refs/tags/' + tagName,
+        sha: createdTag.data.sha
+    }).then(
+        ({ status }) => {
+            if (status !== 201) {
+                core.setFailed(`Could not create reference. Received ${status} from API`)
+            }
+        }
+    )
 }
+
+tag().catch(
+    (err) => {
+        core.setFailed(err.message)
+    }
+);
